6274e774e01f9bf336dff636a6d95b91d22d1e30
Austin Pechan <agp@berkeley.edu>
2023-09-16 00:11:22 -0700

    made it work for reading all three needed information bits. Also made images more clear

--- main.py
+++ main.py
@@ -1,47 +1,51 @@
 import pytesseract as tes
-from PIL import Image, ImageOps
+from PIL import Image, ImageOps, ImageFilter
 import cv2
 import numpy as np
+import math
 import regex as re
 tes.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
 
 
 def main():
-    image = Image.open('One_ASIC_Image.jpg')
+    image = Image.open('COLDATA.jpg')
 
-    im = convert_image(image)
+    width, height = image.size
+
+    #arbitrary parameters
+    left = width / 2.53
+    up = height / 2.9
+    right = width / 2.08
+    down = height / 2.5
+
+    im = convert_image(image, left, right, up, down)
 
     # Convert the image to text
     text = tes.pytesseract.image_to_string(im)
 
-    # Parses out everything but numbers 0-9
-    regex = re.compile(r'[^0-9]')
-    text = regex.sub('', text)
+    # [0] = type of chip, [2] = serial_number, [3] = lot number
+    text = text.split("\n")
 
-    print("ASIC_Serial_Number: " + text)
+    print("type_of_chip: " + text[0] + ", " + "serial_number: " + text[2] + ", " + "lot_number: " + text[3])
 
 
-def convert_image(im):
+def convert_image(im, left, right, up, down):
     # Size of im in pixels
-    width, height = im.size
-    
-    left = width / 6
-    top = height / 2.2
-    right = width / 2.25
-    bottom = height / 1.94
     
     # Crop
-    im1 = im.crop((left, top, right, bottom))
+    im1 = im.crop((left, up, right, down))
 
     # Resize
-    im1.resize((width * 2, height * 2))
+    im1.resize((im.width * 4, im.height * 4))
+
+    im1 = im1.convert('L')
 
     # Convert to Binary Image
-    (thresh, BWimg) = cv2.threshold(np.array(im1), 100, 255, cv2.THRESH_BINARY)
+    (thresh, BWimg) = cv2.threshold(np.array(im1), 117, 255, cv2.THRESH_BINARY)
     im1 = Image.fromarray(BWimg)
 
-    #Invert Image
-    #im1 = ImageOps.invert(im1)
+    # Invert Image
+    im1 = ImageOps.invert(im1)
 
     # Shows the image in image viewer
     #im1.show()
