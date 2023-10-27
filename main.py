b4df7b3f105b27b6b5a2891bac74b2e4ecdfc831
Austin Pechan <agp@berkeley.edu>
2023-10-26 22:11:19 -0700

    added some code for the new images

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pytesseract as tes
-from PIL import Image, ImageOps, ImageFilter
+from PIL import Image, ImageOps, ImageFilter, ImageEnhance
 import cv2
 import numpy as np
 import regex as re
@@ -19,7 +19,7 @@ else:
 
 
 def main():
-    image = Image.open('COLDATA.jpg')
+    image = Image.open('DUNE-Reader/ColdADC_test_images/Full_test_1.jpg')
 
     width, height = image.size
 
@@ -31,11 +31,11 @@ def main():
     # right = width
     # down = height / 1.4
 
-    # parameters for COLDATA.jpg
-    left = width / 2.53
-    up = height / 2.9
-    right = width / 2.08
-    down = height / 2.5
+    # parameters for DUNE-Reader/COLDATA.jpg
+    # left = width / 2.53
+    # up = height / 2.9
+    # right = width / 2.08
+    # down = height / 2.5
 
     # parameters for One_Asic (note this does not work as the image is poor quality. Only used for testing)
     # left = width / 6
@@ -43,31 +43,38 @@ def main():
     # right = width / 1.35
     # down = height / 1.7
 
-    im = convert_image(image, left, right, up, down)
+    # im = convert_image(image, left, right, up, down)
 
-    # Convert the image to text
-    text_output(im)
+    # # Convert the image to text
+    # text_output(im)
+
+    full_test_1(image)
 
 
 def convert_image(im, left, right, up, down):
-    # Size of im in pixels
-    
+
     # Crop
     im1 = im.crop((left, up, right, down))
 
     # Resize
     im1.resize((im.width * 4, im.height * 4))
 
+    im1 = im1.filter(ImageFilter.BoxBlur(1))
+    
+    # Contrast
+    enhancer = ImageEnhance.Contrast(im1)
+    im1 = enhancer.enhance(4.5)
+
     # Grayscale
     im1 = im1.convert('L')
 
-    # kernel_set = [1, 4, 1,
-    #               4, 12, 4,
-    #               1, 4, 1]
-    # kernel = ImageFilter.Kernel(size=(3, 3), kernel=kernel_set)
+    kernel_set = [1, 4, 1,
+                  4, 12, 4,
+                  1, 4, 1]
+    kernel = ImageFilter.Kernel(size=(3, 3), kernel=kernel_set)
 
-    # # Apply the filter to the image
-    # im1 = im1.filter(kernel)
+    # Apply the filter to the image
+    im1 = im1.filter(kernel)
 
     # Convert to Binary Image
     (thresh, BWimg) = cv2.threshold(np.array(im1), 117, 255, cv2.THRESH_BINARY)
@@ -76,23 +83,23 @@ def convert_image(im, left, right, up, down):
     # Invert Image
     im1 = ImageOps.invert(im1)
 
-    im2 = im1.filter(ImageFilter.BoxBlur(1))
-
     # Load the image
-    image = np.array(im2)
+    # image = np.array(im1)
 
-    # Denoise the image
-    denoised_image = denoise_tv_bregman(image, 5)
-    threshold_value = filters.threshold_otsu(denoised_image)
-    binary_image = denoised_image > threshold_value
+    # # Denoise the image
+    # denoised_image = denoise_tv_bregman(image, 5)
+    # threshold_value = filters.threshold_otsu(denoised_image)
+    # binary_image = denoised_image > threshold_value
 
-    # Perform binary dilation on the binary image
-    dilated_image = morphology.binary_dilation(binary_image)
+    # # Perform binary dilation on the binary image
+    # dilated_image = morphology.binary_dilation(binary_image)
 
 
-    pil_image = Image.fromarray((dilated_image * 255).astype(np.uint8))
-    # Shows the image in image viewer
-    pil_image.show()
+    # pil_image = Image.fromarray((dilated_image * 255).astype(np.uint8))
+    # # Shows the image in image viewer
+    # pil_image.show()
+
+    im1.show()
     return im1
 
 
@@ -103,7 +110,7 @@ def text_output(im):
     # [0] = type of chip, [2] = serial_number, [3] = lot number
     text = text.split("\n")
 
-    print(text)
+    #print(text)
 
     # sparse out empty array elements
     text = [x for x in text if x]
@@ -114,7 +121,22 @@ def text_output(im):
 
     type_of_chip = get_close_matches(text[0], ["LArASIC", "ColdADC", "COLDATA"], 1)
 
-    print("type_of_chip: " + type_of_chip[0] + ", " + "serial_number: " + text[2] + ", " + "lot_number: " + text[3])
+    final_text = "type_of_chip: " + type_of_chip[0] + ", " + "serial_number: " + text[2] + ", " + "lot_number: " + text[3]
+
+    print(final_text)
+    return(final_text)
+
+
+def full_test_1(image):
+    width, height = image.size
+    image =  image.rotate(270)
+    #im_1 = convert_image(image, width/2.55, width/2.25, height/3.4, height/2.7)
+    im_2 = convert_image(image, width/2.55, width/2.25, height/1.66, height/1.52)
+
+    print(tes.pytesseract.image_to_string(im_2))
+
+#     return LArASIC_1, LArASIC_2, LArASIC_3, LArASIC_4, 
+# ColdADC_1, ColdADC_2, ColdADC_3, ColdADC_4, ColdATA_1, ColdATA_2
 
 
 if __name__ == "__main__":
