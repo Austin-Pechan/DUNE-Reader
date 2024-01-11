f5565f00b8c9509660d79f7ddb8744a87ce0cd01
Austin Pechan <agp@berkeley.edu>
2024-01-11 12:00:54 -0800

    worked on text read out post processing

--- main.py
+++ main.py
@@ -21,9 +21,10 @@ else:
 def convert_image(im):
     im = Image.fromarray(im)
     enhancer = ImageEnhance.Contrast(im)
-    im = enhancer.enhance(5)
+    im = enhancer.enhance(30)
     im = im.convert('L')
     im = np.array(im)
+    _, im = cv2.threshold(im, 215, 255, cv2.THRESH_BINARY)
 
     resize_factor = 1
     im = cv2.resize(im, (0, 0), fx=resize_factor, fy=resize_factor)
@@ -37,62 +38,56 @@ def convert_image(im):
     im1 = im1.filter(ImageFilter.SHARPEN)
 
 
-    # # Contrast
-    # enhancer = ImageEnhance.Contrast(im1)
-    # im1 = enhancer.enhance(5)
-
-    # # Grayscale
-    # im1 = im1.convert('L')
-    # kernel_set = [3, 6, 3,
-    #               6, 24, 6,
-    #               3, 6, 3]
-    # kernel = ImageFilter.Kernel(size=(3, 3), kernel=kernel_set)
-
-    # # Apply the filter to the image
-    # im1 = im1.filter(kernel)
-
-    # # Convert to Binary Image
-    # (thresh, BWimg) = cv2.threshold(np.array(im1), 117, 255, cv2.THRESH_BINARY)
-    # im1 = Image.fromarray(BWimg)
-
 
     # Invert Image
     im1 = ImageOps.invert(im1)
 
-    # # Denoise the image
-    # im2 = np.array(im1)
-    # denoised_image = denoise_tv_bregman(im2, 5)
-    # threshold_value = filters.threshold_otsu(denoised_image)
-    # binary_image = denoised_image > threshold_value
-
-    # # Perform binary dilation on the binary image
-    # dilated_image = morphology.binary_dilation(binary_image)
-
-
-    # pil_image = Image.fromarray((dilated_image * 255).astype(np.uint8))
-
-    # pil_image.show()
-
-    im1.show()
+    # im1.show()
     return im1
 
 
 def text_output(im):
-    custom_config = r'--psm 3'
+    custom_config = r'--psm 6'
     text = tes.image_to_string(im, lang='eng', config=custom_config)
 
     # [0] = type of chip, [2] = serial_number, [3] = lot number
     text = text.split("\n")
 
     #sparse out empty array elements
-    text = [x for x in text if x]
+    text = [re.sub('[^a-zA-Z0-9/-]', '', x) for x in text if x.strip() != '']
+    text = [re.sub('[^a-zA-Z0-9/-]', '', x) for x in text if x.strip() != '']
 
-    # # Sparse code to remove bad info
-    # text[2] = re.sub('[^a-zA-Z0-9]', '', text[2])
-    # text[3] = re.sub('[^a-zA-Z0-9]', '', text[3])
+    larasic = None
+    for i in range(len(text)):
+        if get_close_matches(text[i], ["ColdADC", "COLDATA"], cutoff=0.5):
+            larasic = False
+            break
+        elif get_close_matches(text[i], ["LArASIC"], cutoff=0.5):
+            larasic = True
+            break
 
-    # type_of_chip = get_close_matches(text[0], ["LArASIC", "ColdADC", "COLDATA"], 1)
-    # final_text = "type_of_chip: " + type_of_chip[0] + ", " + "serial_number: " + text[2] + ", " + "lot_number: " + text[3]
+    bnl = True 
+    lar = True
+    if larasic:
+        i = 0
+        while i < len(text):
+            if bnl:
+                close_matches = get_close_matches(text[i], ["BNL"], cutoff=0.5)
+                if close_matches and close_matches[0] == "BNL":
+                    bnl = False
+                    text[i] = 'BNL'
+                else:
+                    text.pop(i)
+                    i -= 1
+            elif lar:
+                close_matches_lar = get_close_matches(text[i], ["LArASIC"], cutoff=0.5)
+                if close_matches_lar and close_matches_lar[0] == "LArASIC":
+                    lar = False
+                    text[i] = 'LArASIC'
+                else:
+                    text.pop(i)
+                    i -= 1
+            i += 1
 
     print(text)
     return(text)
